5ef335c6a6820858434e39f3fc2ca416d4c16106
Kevin <kevin.mahoney@zenotec.net>
2014-12-09 15:03:37 -0700

    Create subdirectories for each module
    Signed-off-by: Kevin <kevin.mahoney@zenotec.net>

---
+++ inc/zutils/zException.h
@@ -0,0 +1,35 @@
+//*****************************************************************************
+//    Copyright (C) 2014 ZenoTec LLC (http://www.zenotec.net)
+//
+//    File: zException.h
+//    Description:
+//
+//*****************************************************************************
+
+#ifndef __ZEXCEPTION_H__
+#define __ZEXCEPTION_H__
+
+#include <exception>
+
+namespace zUtils
+{
+namespace zException
+{
+
+class Exception : public std::exception
+{
+public:
+  Exception();
+  ~Exception();
+
+protected:
+
+private:
+
+
+};
+}
+}
+
+
+#endif /* __ZEXCEPTION_H__ */
---
+++ src/zException/Exception.cpp
@@ -0,0 +1,30 @@
+//*****************************************************************************
+//    Copyright (C) 2014 ZenoTec LLC (http://www.zenotec.net)
+//
+//    File: zException.cpp
+//    Description:
+//
+//*****************************************************************************
+
+#include "zutils/zException.h"
+
+namespace zUtils
+{
+namespace zException
+{
+
+Exception::Exception()
+{
+
+}
+
+Exception::~Exception()
+{
+
+}
+
+}
+}
+
+
+
