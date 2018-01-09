0ae3b8aff82651f0bc8aac3cea7fabb5f844eb73
Kevin Mahoney <k.mahoney@cablelabs.com>
2018-01-09 15:32:19 -0700

    zWireless: Removed parsing of empty message
    Signed-off-by: Kevin Mahoney <k.mahoney@cablelabs.com>

--- lib/zWireless/nl80211/StartApCommand.cpp
+++ lib/zWireless/nl80211/StartApCommand.cpp
@@ -192,17 +192,6 @@ int
 StartApCommand::ack_cb(struct nl_msg* msg_, void* arg_)
 {
 
-  GenericMessage msg(msg_);
-  if (!msg.Parse())
-  {
-    ZLOG_ERR("Error parsing generic message");
-    return (NL_SKIP);
-  }
-
-//  std::cout << "StartApCommand::ack_cb()" << std::endl;
-//  msg.Display();
-//  msg.DisplayAttributes();
-
   this->_status = true;
   this->_count.Post();
 
