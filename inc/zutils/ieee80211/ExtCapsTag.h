2ffe2be921ce2aca8bb059c0296b0a02617a694b
Kevin Mahoney <k.mahoney@cablelabs.com>
2018-04-19 14:53:41 -0600

    zWireless: Added extended capabilities tag class
    Signed-off-by: Kevin Mahoney <k.mahoney@cablelabs.com>

---
+++ inc/zutils/ieee80211/ExtCapsTag.h
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2017 Cable Television Laboratories, Inc. ("CableLabs")
+ *                    and others.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __IEEE80211_EXTENDEDCAPSTAG_H__
+#define __IEEE80211_EXTENDEDCAPSTAG_H__
+
+#include <zutils/ieee80211/Tag.h>
+
+namespace zUtils
+{
+namespace zWireless
+{
+namespace ieee80211
+{
+
+struct ext_caps_tag
+{
+  uint8_t caps[8];
+};
+
+//*****************************************************************************
+// Class: ExtCapsTag
+//*****************************************************************************
+
+class ExtCapsTag : public Tag
+{
+
+public:
+
+  ExtCapsTag() :
+    Tag(Tag::ID_DSSS, sizeof(struct ext_caps_tag))
+  {
+  }
+
+  virtual
+  ~ExtCapsTag()
+  {
+  }
+
+  struct ext_caps_tag
+  operator()() const
+  {
+    struct ext_caps_tag val = { 0 };
+    this->GetValue(val);
+    return(val);
+  }
+
+  bool
+  operator()(const struct ext_caps_tag caps_)
+  {
+    return(this->PutValue(caps_));
+  }
+
+  virtual void
+  Display() const
+  {
+    Tag::Display();
+  }
+
+protected:
+
+private:
+
+};
+
+}
+}
+}
+
+#endif /* __IEEE80211_EXTENDEDCAPSTAG_H__ */
