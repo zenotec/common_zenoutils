d52b289edc1fe4be29d13321a759adfe45ede4d1
wglennon <williamacglennon@gmail.com>
2018-06-07 11:07:25 -0600

    sdfg

--- test/zWireless/UnitTest.cpp
+++ test/zWireless/UnitTest.cpp
@@ -50,7 +50,7 @@ main(int argc, const char **argv)
   UTEST_TEST(zWirelessTest_MonitorInterface, 0);
   UTEST_TEST(zWirelessTest_AccessPointInterface, 0);
   UTEST_TEST(zWirelessTest_setFrequency, 0);
-
+  UTEST_TEST(zWirelessTest_getCenterFrequency1, 0);
   UTEST_TEST(zWirelessTest_BasicServiceSet, 0);
 
   zLog::Manager::Instance().UnregisterConnector(zLog::Log::LEVEL_ALL);
