3b12a6edafcb0011101e239abf7facd4307a020a
Steve Arendt <s.arendt@cablelabs.com>
2018-12-10 06:45:56 -0700

    Added install of essential libs.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+sudo apt-get install libtool
+sudo apt-get install uuid-dev
+sudo apt-get install libnl-3-dev
+sudo apt-get install libnl-genl-3-dev
+sudo apt-get install libnl-route-3-dev
+sudo apt-get install libboost-all-dev
+
 mkdir -p ./build ./config ./m4
 touch NEWS README AUTHORS ChangeLog
 autoreconf --force --install -I config -I m4
