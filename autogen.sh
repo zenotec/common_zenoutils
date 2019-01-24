4f83f9c8d15eb4dc22462146fa78e9a0ad049cb5
Zack Foreman <z.foreman@cablelabs.com>
2019-01-24 08:59:05 -0700

    added more library checks

--- autogen.sh
+++ autogen.sh
@@ -7,6 +7,10 @@ sudo apt-get install libnl-genl-3-dev
 sudo apt-get install libnl-route-3-dev
 sudo apt-get install libboost-all-dev
 sudo apt-get install libssl-dev
+sudo apt-get install libcrypto++-dev
+sudo apt-get install libcrypto++-utils
+sudo apt-get install libcrypto++6
+sudo apt-get install libcrypto++6-dbg
 
 mkdir -p ./build ./config ./m4
 touch NEWS README AUTHORS ChangeLog
